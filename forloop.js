42cdc79873aa1526f58f87c226f617abef76725e
Wally <Wally@Wallys-MBP.home>
2020-10-13 00:51:27 -0400

    adding for loop

---
+++ forloop.js
@@ -0,0 +1,27 @@
+//for(initialization;condition;increment)
+
+for(var counter=0;counter<10;counter++)
+{
+    console.log(counter)
+    console.log("hello") 
+
+}
+
+console.log("outside")
+
+for(var counter=0;counter<10;counter=counter+2)
+{
+    console.log(counter)
+    console.log("hello") 
+
+}
+
+console.log("while lopp")
+var i=0 //initialize
+while(i<10) //condition
+{
+
+    console.log(i)
+    console.log("hello")    
+    i++
+}
